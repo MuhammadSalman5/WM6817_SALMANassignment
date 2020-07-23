fae231aa2a5a4f09bb13cc9fdba264d762e608e8
MuhammadSalman5 <57804600+MuhammadSalman5@users.noreply.github.com>
2020-07-24 00:53:29 +0500

    Add files via upload

---
+++ Remaining Questions(43-48)/Question 2/app.js
@@ -0,0 +1,16 @@
+function samsung()
+{
+    alert("This is samsung mobile");
+}
+function huawe()
+{
+    alert("This is Huawe Mobile");
+}
+function infinix()
+{
+    alert("This is Infinix Mobile");
+}
+function oppo()
+{
+    alert("This is Oppo Mobile");
+}
---
+++ Remaining Questions(43-48)/Question1/app.js
@@ -0,0 +1,4 @@
+function display()
+{
+    alert("This is Link alert");
+}
